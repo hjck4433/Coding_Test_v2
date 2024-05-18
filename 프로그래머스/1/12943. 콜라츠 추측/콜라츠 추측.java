b042a19fc00c7d2edba795fb5663e6c3953870b2
HYUNJI <84636849+hjck4433@users.noreply.github.com>
2024-05-19 00:54:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        if (num == 1) return 0;
+        
+        while(true){
+            answer++;
+            if(num % 2 == 0) {
+                num /= 2;
+            }else {
+                num = num * 3 +1;
+            }
+            if (num == 1) break;
+            if(answer == 500) return -1;
+        }
+        
+        return answer;
+    }
+}

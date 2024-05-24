28bff6b1b491be6a110e9eb116bac42e5e888550
HYUNJI <84636849+hjck4433@users.noreply.github.com>
2024-05-24 16:03:00 +0900

    [Silver IV] Title: 괄호, Time: 232 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void foo(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        int i = 0;
+        while(i < s.length()) {
+            char c = s.charAt(i);
+            
+            if (c=='(') {
+                stack.push(c);
+            }else {
+                if (stack.size() < 1) {
+                    System.out.println("NO");
+                    return;
+                }
+                stack.pop();
+            }
+            
+            i++;
+        }
+        if (stack.size() > 0) System.out.println("NO");
+        else System.out.println("YES");
+    }
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            foo(sc.next());
+        }
+    }
+}

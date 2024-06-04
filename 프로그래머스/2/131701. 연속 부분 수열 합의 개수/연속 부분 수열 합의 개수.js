4196a623bb9236c5ba50058173aa313f012017e2
HYUNJI <84636849+hjck4433@users.noreply.github.com>
2024-06-04 16:02:17 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 86.00 ms, Memory: 56.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,21 @@
+function solution(elements) {
+
+    let nums = new Set();
+    
+    for(let i = 1; i <= elements.length; i++){
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++){
+            if(j === 0){
+                for (let k = 0; k<i; k++){
+                    sum += elements[k];
+                }
+            }else {
+                sum -= elements[j-1];
+                sum += elements[(j+i-1) % elements.length];
+            }
+            nums.add(sum);
+        }
+    }
+    
+    return nums.size;
+}
